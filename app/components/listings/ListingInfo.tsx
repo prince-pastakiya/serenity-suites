f7732038fe5acd8b18d525d8bb96838597f60c53
prince-pastakiya <princepastakiya@gmail.com>
2024-06-16 00:51:00 -0500

    Listing Info Changed

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -44,7 +44,7 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
 			<div className="flex flex-col gap-2">
 				<div className="text-xl font-semibold flex flex-row items-center gap-2">
 					<div>Hosted by {user?.name}</div>
-					<Avatar src={user?.image} />
+					<Avatar src="placeholder.png" />
 				</div>
 				<div className="flex flex-row items-center gap-4 font-light text-neutral-500">
 					<div>{guestCount} Guests</div>
